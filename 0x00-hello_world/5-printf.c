7d46d8e81cecf2d75443b9ae7ceb5d4355d654c1
Romeo509 <romeoameku503@gmail>
2023-08-10 11:15:08 -0700

    lets use the printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * My wonderful comments
+ * Return: Always 0 As usual
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n")
+    return (0);
+}
