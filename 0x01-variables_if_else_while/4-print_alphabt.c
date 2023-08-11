a510afeefa1e5f0a275489c0b0663945f562ea7e
Romeo509 <romeoameku503@gmail>
2023-08-11 16:00:06 -0700

    hmm Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alpha;
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+{
+if (alpha != 'q' && alpha != 'e')
+{
+putcher{alpha};
+}
+}
+putcher('\n');
+return (0);
+}
