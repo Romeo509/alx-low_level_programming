d312c53e8b01f9a10f77030f899d444e6f7caeae
Romeo509 <romeoameku503@gmail>
2023-08-11 16:37:44 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int integer;
+for (integer = 0 ; integer < 10 ; integer++)
+{
+printf("%d", integer);
+}
+putchar('\n');
+return (0);
+}
