f22e886e0a772d7dd484e70fde653cef43628b61
Romeo509 <romeoameku503@gmail>
2023-08-11 11:48:09 -0700

    hope this is well

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+putchar(alphabet);
+}
+putchar('\n')
+return (0)
