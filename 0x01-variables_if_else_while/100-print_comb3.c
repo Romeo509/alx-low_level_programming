92cbad490f1e576ab89049f56d2f61cefef019ca
Romeo509 <romeoameku503@gmail>
2023-08-12 09:35:44 -0700

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,18 @@
 int main(void)
 {
 int i, j;
-for (i = 0 ; i <= 8; i++)
+for (i = 0; i <= 8; i++)
 {
-for (j = 1; j <= 9; j++)
+for (j = i + 1; j <= 9; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
-if (i != 8 || j != 9)
+if (i == 8 && j == 9)
 {
+continue;
+}
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
