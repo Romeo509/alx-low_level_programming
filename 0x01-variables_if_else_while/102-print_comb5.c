06ec9830ead4f3e9eb9cba907ecbf1a5487ee157
Romeo509 <romeoameku503@gmail>
2023-08-12 10:27:35 -0700

    The software is already my lunch

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,14 +12,14 @@ int main(void)
 int i, j;
 for (i = 0; i <= 99; i++)
 {
-for (j = i; j <= 99; j++)
+for (j = i + 1; j <= 99; j++)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
 putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
-if (i != 99 || j != 99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
