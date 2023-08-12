bb52e66765b3e32cccb5e7f1aff0278b5d1bbbd3
Romeo509 <romeoameku503@gmail>
2023-08-11 17:10:35 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main -Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int integer;
+for (integer = '0' ; integer <= '9' ; integer++)
+{
+putcher(integer);
+}
+putcher('\n');
+return (0);
+}
