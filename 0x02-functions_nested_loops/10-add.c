5d436b42e153a9d398b0394761ec42478391907f
Romeo509 <romeoameku503@gmail>
2023-08-15 23:36:35 +0100

    print and add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add -prints and add two numbers
+*@a: print int a
+*@b: print int b
+* Return: Always 0.
+*/
+int add(int a, int b)
+{
+return a + b;
+}
