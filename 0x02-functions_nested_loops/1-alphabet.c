2abd9f439bf4bbe355db299ca02c9c27bd88d4ba
Romeo509 <romeoameku503@gmail>
2023-08-15 15:41:04 +0100

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Return: 0
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+  _putchar(letter);
+ }
+ _putchar('\n')
+}
