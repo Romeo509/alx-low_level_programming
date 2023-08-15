4a03c5fd3aa60f2211f523643fd21dd3735b5f5e
Romeo509 <romeoameku503@gmail>
2023-08-15 23:17:56 +0100

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,11 +7,11 @@
 */
 void times_table(void) 
 int row, column, product;
-for (row = 0; rom <= 9; row++)
+for (row = 0; row <= 9; row++)
 {
 for (column = 0; column <= 9; column++)
 {
-product = romw * column;
+product = row * column;
 if (column != 0)
 {
 _putchar(',')
