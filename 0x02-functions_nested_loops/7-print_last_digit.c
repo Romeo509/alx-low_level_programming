5d8058cb904ec4542a5906b584f253c9b438847c
Romeo509 <romeoameku503@gmail>
2023-08-15 22:48:29 +0100

    Lets print the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,12 +13,12 @@ if (n < 0)
 {
 last_digit = -1 * (n % 10);
 _putchar(last_digit + '0');
-return last_digit;
+return (last_digit);
 }
 else
 {
 last_digit = n % 10;
 _putchar(last_digit + '0');
-return last_digit;
+return (last_digit);
 }
 }
