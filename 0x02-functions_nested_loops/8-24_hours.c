195dfefc78b5e77b1e49a201f011f537f50f7f18
Romeo509 <romeoameku503@gmail>
2023-08-15 22:58:59 +0100

    print every moment of his life in c language

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*jack_bauer - prints every minutes of the day of jack_bauer
+*
+*Return: Always 0.
+*/
+void jack_bauer(void)
+{
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minut < 60; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+}
+}
