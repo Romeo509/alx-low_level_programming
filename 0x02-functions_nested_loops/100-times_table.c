ee51e68ba7b3a4ab3e1acc307367bae580e9f21e
Romeo509 <romeoameku503@gmail>
2023-08-16 04:56:24 +0100

    very stressful

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table.
- * @n: The number to start counting from n to 98.
- */
+* print_times_table - Prints the n times table.
+* @n: The number to start the times table from.
+*/
 void print_times_table(int n)
 {
 if (n < 0 || n > 15)
@@ -14,16 +14,18 @@ for (int j = 0; j <= n; j++)
 {
 int product = i * j;
 if (j != 0)
-_putchar(',');
+_putchar(',');           
+_putchar(' ');
 if (product < 10)
 _putchar(' ');
 else if (product < 100)
-_putchar(' ');
-_putchar('0' + product / 100);
-_putchar('0' + (product / 10) % 10);
-_putchar('0' + product % 10);
-if (j != n)
-_putchar(' ');
+_putchar((product / 10) + '0');
+else
+{
+_putchar((product / 100) + '0');
+_putchar(((product / 10) % 10) + '0');
+}
+_putchar((product % 10) + '0');
 }
 _putchar('\n');
 }
