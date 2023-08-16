23e39327183f18b6caa2d4248061a24bb79e596c
Romeo509 <romeoameku503@gmail>
2023-08-16 06:09:58 +0100

    alskd

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
-* putnchar - Print a number of any digit count
-* @num: The number to print
-*/
-void putnchar(int num)
-{
-if (num < 10)
-_putchar(num + '0');
-else
-{
-putnchar(num / 10);
-_putchar(num % 10 + '0');
-}
-}
-/**
 * print_times_table - Print multiplication table for factor of n
 * @n: Integer n
 */
@@ -26,13 +12,51 @@ for (int i = 0; i <= n; i++)
 {
 for (int j = 0; j <= n; j++)
 {
-int prod = i * j;
-putnchar(prod);
-if (j != n)
+int product = i * j;
+
+if (j != 0)
 {
-_putchar(',');
-for (int m = 0; m < 4 - numLength(i * (j + 1)); m++)
+_putchar(' ');
+if (product < 10)
+ _putchar(' ');
 _putchar(' ');
 }
+
+if (product < 10)
+_putchar('0' + product);
+else if (product < 100)
+{
+_putchar('0' + product / 10);
+_putchar('0' + product % 10);
 }
+else
+{
+_putchar('0' + product / 100);
+_putchar('0' + (product % 100) / 10);
+_putchar('0' + product % 10);
+}
+
+if (j != n)
+_putchar(',');
+else
 _putchar('\n');
+}
+}
+}
+
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+print_times_table(3);
+_putchar('\n');
+print_times_table(5);
+_putchar('\n');
+print_times_table(98);
+_putchar('\n');
+print_times_table(12);
+return (0);
+}
