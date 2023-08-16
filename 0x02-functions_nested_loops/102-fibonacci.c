a36145370631ee0ee727b748274c4bbd20592237
Romeo509 <romeoameku503@gmail>
2023-08-16 06:29:22 +0100

    print first 50 words

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - prints the first 50 numbers
+*
+*return: 0
+*/
+int main(void)
+{
+int i, n;
+long f1 = 1, f2, next;
+f1 = 1;
+f2 = 2;
+n = 50;
+for (i = 0; i < n; i++)
+{
+if (i == 49)
+{
+printf("%lu", f1);
+} else
+{
+printf("%lu, ", f1);
+next = f1 + f2;
+f1 = f2;
+f2 = next;
+}
+}
+printf("\n");
+return (0);
+}
