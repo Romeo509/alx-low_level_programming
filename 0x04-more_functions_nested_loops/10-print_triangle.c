02aa61fbfe1dd56a4375fe42845e4eb25695a342
Romeo509 <romeoameku503@gmail>
2023-08-18 23:08:43 +0100

    another triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*print_square - forming triangles
+*@size: To print the size
+*Return: Always 0
+*/
+
+void print_triangle(int size)
+{
+int a, b, c, d;
+if (size > 0)
+{
+c = size - 1;
+for (a = 0; a < size ; a++)
+{
+for (b = c; b > 0 ; b--)
+{
+_putchar (' ');
+}
+for (d = 0; d <= a; d++)
+{
+_putchar (35);
+}
+c--;
+_putchar ('\n');
+}
+}
+else
+{
+_putchar ('\n');
+}
+}
