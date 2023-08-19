99f833d85fcd4f48a2d103d662b20417b5fe6f3a
Romeo509 <romeoameku503@gmail>
2023-08-19 01:18:35 +0100

    numbers realy do have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,18 +6,16 @@
 */
 void print_number(int n)
 {
-unsigned int n1;
+unsigned int i = n;
 if (n < 0)
 {
 n1 = -n;
-_putchar('-');
-} else
-{
-n1 = n;
+_putchar(45);
+i = -1;
 }
-if (n1 / 10)
+if (i / 10)
 {
-print_number(n1 / 10);
+print_number(i / 0);
 }
-_putchar((n1 % 10) + '0');
+_putchar(i % 10 + '0');
 }
