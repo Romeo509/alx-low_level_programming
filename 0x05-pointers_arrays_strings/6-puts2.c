e87db8ffa273cd283e16cdf497f03d64f121d6ca
Romeo509 <romeoameku503@gmail>
2023-08-21 21:50:49 +0100

    expectation should be correct for now

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,9 @@ int i = 0;
 while (str[i] != '\0')
 {
 _putchar(str[i]);
-i += 2;
+i++;
+if (str[i] != '\0') // Check if the next character is not the null-terminator
+i++;            // Increment i again to skip the next character
 }
 _putchar('\n');
 }
