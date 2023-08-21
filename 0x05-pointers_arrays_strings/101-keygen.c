b6984c256f3c6bda50ad24f7b851781571513d5e
Romeo509 <romeoameku503@gmail>
2023-08-22 00:33:52 +0100

    hackers are not born, they are made

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,28 +9,15 @@
 */
 int main(void)
 {
-char password[84];
-int i, sum, random;
-srand(time(0));
-sum = 0;
-for (i = 0; i < 4; i++)
+int sum;
+char c;
+srand(time(NULL));
+while (sum <= 2645)
 {
-random = rand() % 10;
-password[i] = '0' + random;
-sum += random;
+c = rand() % 128;
+sum += c;
+putchar(c);
 }
-password[i++] = 2772 - sum;   
-for (; i < 84; i++)
-{
-random = rand() % 62;
-if (random < 10)
-password[i] = '0' + random;
-else if (random < 36)
-password[i] = 'a' + random - 10;
-else
-password[i] = 'A' + random - 36;
-}
-password[i] = '\0';
-printf("%s\n", password);  
+putchar(2772 - sum);
 return (0);
 }
