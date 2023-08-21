1ba84da2b25e82c19dff550279d51fd3b5a5a432
Romeo509 <romeoameku503@gmail>
2023-08-22 00:26:25 +0100

    hackers are not born, they are made

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,36 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
+* main - Entry point
+*
+* Return: Always 0
 */
 int main(void)
 {
-int num;
-
+char password[84];
+int i, sum, random;
 srand(time(0));
-num = rand();
-printf("%i\n", num);
+sum = 0;
+for (i = 0; i < 4; i++)
+{
+random = rand() % 10;
+password[i] = '0' + random;
+sum += random;
+}
+password[i++] = 2772 - sum;   
+for (; i < 84; i++)
+{
+random = rand() % 62;
+if (random < 10)
+password[i] = '0' + random;
+else if (random < 36)
+password[i] = 'a' + random - 10;
+else
+password[i] = 'A' + random - 36;
+}
+password[i] = '\0';
+printf("%s\n", password);  
 return (0);
 }
