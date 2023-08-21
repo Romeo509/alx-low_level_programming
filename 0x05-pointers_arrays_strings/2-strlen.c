ae522caeb1c66fa3616abb1f85f35cc1155a11d5
Romeo509 <romeoameku503@gmail>
2023-08-21 17:41:14 +0100

    The risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen - defines the length of the string
+*@s: Represents the pointer that will be changed
+*Return: the corrects answer
+*/
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+lenght++;
+s++;
+}
+return length;
+}
