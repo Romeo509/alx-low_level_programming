00a2fcb149a57d772a118ff658c8e973377a9b67
Romeo509 <romeoameku503@gmail>
2023-08-21 21:33:40 +0100

    sld

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,12 @@
 
 void rev_string(char *s)
 {
-int length = 0;
-char temp;
-while (s[length] != '\0')
+int i = 0;
+while (s[i])
+i++;
+while (i--)
 {
-length++;
+_putchar(s[i]);
 }
-for (int i = 0; i < length / 2; i++)
-{
-temp = s[i];
-s[i] = s[length - i - 1];
-s[length - i - 1] = temp;
-}
+_putchar('\n');
 }
