ea1d517ef77274b34530393a2324bd41c7a51f56
Romeo509 <romeoameku503@gmail>
2023-08-25 00:31:50 +0100

    moicy buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,6 +9,11 @@
 void print_buffer(char *b, int size)
 {
 int i, j;
+if (size <= 0)
+{
+printf("\n");
+return;
+}
 for (i = 0; i < size; i += 10)
 {
 printf("%08x: ", i);
@@ -19,12 +24,14 @@ printf("%02x", (unsigned char)b[i + j]);
 if (j % 2 == 1)
 printf(" ");
 }
+printf(" ");
 for (j = 0; j < 10; j++)
 {
 if (i + j < size)
 {
-if (b[i + j] >= 32 && b[i + j] <= 126)
-printf("%c", b[i + j]);
+char c = b[i + j];
+if (isprint(c))
+printf("%c", c);
 else
 printf(".");
 }
