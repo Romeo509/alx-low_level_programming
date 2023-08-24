7a38701e905ed2f8c82e1d5c2c91686c8027d5ba
Romeo509 <romeoameku503@gmail>
2023-08-24 16:51:08 +0100

    asdf

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,36 @@
 #include "main.h"
 
 /**
-* cap_string - Capitalizes all words of a string.
-* @s: The input string.
+* cap_string - Capitalization of words in a string.
+* @s: The input
 *
 * Return: A pointer to the modified string.
 */
+
 char *cap_string(char *s)
 {
-int count = 0;
+int count = 0, i;
 int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-if (*(s + count) >= 'a' && *(s + count) <= 'z')
-*(s + count) = *(s + count) - ('a' - 'A');
+
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
 count++;
 while (*(s + count) != '\0')
 {
-int separator_found = 0;
-for (int i = 0; i < 13; i++)
+for (i = 0; i < 13; i++)
 {
+
 if (*(s + count) == separators[i])
 {
-separator_found = 1;
-if (*(s + (count + 1)) >= 'a' && *(s + (count + 1)) <= 'z')
-*(s + (count + 1)) = *(s + (count + 1)) - ('a' - 'A');
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+
 break;
 }
 }
-if (separator_found == 0 && *(s + count) >= 'A' && *(s + count) <= 'Z')
-*(s + count) = *(s + count) +('a' - 'A');
+
 count++;
 }
 return (s);
 }
+
