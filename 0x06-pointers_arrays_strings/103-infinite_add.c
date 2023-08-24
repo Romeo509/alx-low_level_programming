4a4f5976cf3bd200918fc37da15f1fbd2835bb74
Romeo509 <romeoameku503@gmail>
2023-08-25 00:49:04 +0100

    add two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,47 @@
 #include "main.h"
-/**
-* infinite_add - adds the numbers
-* @n1: num1
-* @n2: num2
-* @r: result
-* @size_r: size
-*
-* Return: address of r or 0
-*/
 
+/**
+* infinite_add - Adds two numbers.
+* @n1: The first number string.
+* @n2: The second number string.
+* @r: The buffer to store the result.
+* @size_r: The size of the result buffer.
+*
+* Return: A pointer to the result or 0 if the result cannot fit in r.
+*/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1 = 0, len2 = 0, carry = 0, sum;
-char *tmp_r = r;
+int len1 = 0, len2 = 0, carry = 0;
+int i, sum, digit_sum;
 while (n1[len1] != '\0')
 len1++;
 while (n2[len2] != '\0')
 len2++;
-if (len1 > size_r - 1 || len2 > size_r - 1)
-return 0;
-n1 += len1 - 1;
-n2 += len2 - 1;
-r += size_r - 1;
-*r = '\0';
+if (len1 >= size_r || len2 >= size_r)
+return (0);
+r[size_r - 1] = '\0';
+while (len1 > 0 || len2 > 0)
+{
 len1--;
 len2--;
-while (len1 >= 0 || len2 >= 0 || carry)
-{
-sum = carry;
 if (len1 >= 0)
-{
-sum += *n1 - '0';
-n1--;
-}
+sum = n1[len1] - '0';
+else
+sum = 0;
 if (len2 >= 0)
-{
-sum += *n2 - '0';
-n2--;
-}
+sum += n2[len2] - '0';
+sum += carry;
+digit_sum = sum % 10;
 carry = sum / 10;
-*--r = sum % 10 + '0';
-if ((carry && r == tmp_r) || r < tmp_r)
-return 0;
+r[size_r - 2] = digit_sum + '0';
+size_r--;
 }
-return r;
+if (carry > 0)
+{
+if (size_r == 1)
+return (0);
+r[size_r - 2] = carry + '0';
 }
+return (r + size_r - 1);
+}
+
