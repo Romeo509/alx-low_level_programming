bfe7710bf0e4c5edfad3d5565b2c3cc460ad309c
Romeo509 <romeoameku503@gmail>
2023-08-25 01:53:26 +0100

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,37 +11,37 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len_n1, len_n2, carry = 0;
-int i, j, k, result_digit, digit_sum;
-for (len_n1 = 0; n1[len_n1]; len_n1++)
+int l1, l2, carry = 0;
+int a, b, c, digit_sum, result_digit;
+for (l1 = 0; n1[l1]; l1++)
 ;
-for (len_n2 = 0; n2[len_n2]; len_n2++)
+for (l2 = 0; n2[l2]; l2++)
 ;
-if (len_n1 > size_r || len_n2 > size_r)
+if (l1 > size_r || l2 > size_r)
 return (0);
 carry = 0;
-for (i = len_n1 - 1, j = len_n2 - 1, k = 0; k < size_r - 1; i--, j--, k++)
+for (a = l1 - 1, b = l2 - 1, c = 0; c < size_r - 1; a--, b--, c++)
 {
 result_digit = carry;
-if (i >= 0)
-result_digit += n1[i] - '0';
-if (j >= 0)
-result_digit += n2[j] - '0';
-if (i < 0 && j < 0 && result_digit == 0)
+if (a >= 0)
+result_digit += n1[a] - '0';
+if (b >= 0)
+result_digit += n2[b] - '0';
+if (a < 0 && b < 0 && result_digit == 0)
 {
 break;
 }
 carry = result_digit / 10;
-r[k] = result_digit % 10 + '0';
+r[c] = result_digit % 10 + '0';
 }
-r[k] = '\0';
-if (i >= 0 || j >= 0 || carry)
+r[c] = '\0';
+if (a >= 0 || b >= 0 || carry)
 return (0);
-for (k -= 1, i = 0; i < k; k--, i++)
+for (c -= 1, a = 0; a < c; c--, a++)
 {
-result_digit = r[k];
-r[k] = r[i];
-r[i] = result_digit;
+result_digit = r[c];
+r[c] = r[a];
+r[a] = result_digit;
 }
 return (r);
 }
