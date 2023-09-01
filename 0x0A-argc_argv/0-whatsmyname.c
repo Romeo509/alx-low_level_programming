97e54c6d8d67d60c24de5fd1d919cf5110249927
Romeo509 <romeoameku503@gmail>
2023-09-02 00:45:22 +0100

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - Print the name of the program
+*@argc: arguments
+*@argv: Argument
+*
+*Return: 0
+*/
+int main(int argc, char *argv[]) {
+if (argc > 0) {
+printf("%s\n", argv[0]);
+}
+return (0);
+}
+
