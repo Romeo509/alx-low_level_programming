0415982bda555d239271270aaa37e4d0998d9a76
Romeo509 <romeoameku503@gmail>
2023-09-02 01:01:48 +0100

    sf

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - Print all arguments received.
+*@argc: Number of arguments.
+*@argv: Array of argument strings.
+*
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("Argument %d: %s\n", i, argv[i]);
+}
+return (0);
+}
+
