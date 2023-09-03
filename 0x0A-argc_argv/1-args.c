d79c4552af669275874de9942ea312da4fcedabb
Romeo509 <romeoameku503@gmail>
2023-09-03 11:07:15 +0100

    Silenc is the argument carried out by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - represents the type of function
+*@argc: Counts the argument
+*@argv: The argument
+*
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+print("%i\n",argc -1);
+
+return (0);
+}
