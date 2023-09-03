06ef39c6fe34ad23dd833faae1af171593a6553c
Romeo509 <romeoameku503@gmail>
2023-09-03 11:28:38 +0100

    The program adds positive numbers]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,51 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
-* main - prints coins
-* make adassssss
-* @argc: argument
-* @argv: argument
-* Return: 0
+*is_positive_number - Check if a string represents a positive number.
+*@s: The string to check.
+*
+*Return: 1 if it's a positive number, 0 otherwise.
+*/
+int is_positive_number(char *s)
+{
+if (*s == '\0')
+return (0);
+while (*s)
+{
+if (*s < '0' || *s > '9')
+return (0);
+s++;
+}
+return (1);
+}
+/**
+*main - Entry point
+*@argc: Number of command line arguments.
+*@argv: Array of command line arguments.
+*
+*Return: 0 on success, 1 on error.
 */
 int main(int argc, char *argv[])
 {
 int sum = 0;
-if (argc < 2)
+int i;
+if (argc == 1)
 {
 printf("0\n");
 return (0);
 }
-for (int i = 1; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
-int num = atoi(argv[i]);
-if (num == 0 && argv[i][0] != '0')
+if (!is_positive_number(argv[i]))
 {
 printf("Error\n");
 return (1);
 }
-if (num > 0)
-{
-sum += num;
-}
+sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
 return (0);
