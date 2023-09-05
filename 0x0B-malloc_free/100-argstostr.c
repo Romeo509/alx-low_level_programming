41ca2d8e7f349e36a1ff3b5a45ee16255131204c
Romeo509 <romeoameku503@gmail>
2023-09-05 19:43:29 +0100

    Concatenates all the arguments of the program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 
 /**
- * argstostr - Concatenates all the arguments of the program.
- * @ac: The argument count.
- * @av: An array of strings containing the arguments.
- *
- * Return: A pointer to the concatenated string, or NULL on failure.
- */
+*argstostr - Concatenates all the arguments of the program.
+*@ac: The argument count.
+*@av: An array of strings containing the arguments.
+*
+*Return: A pointer to the concatenated string, or NULL on failure.
+*/
 char *argstostr(int ac, char **av)
 {
-if (ac == 0 || av == NULL)
-{
-return (NULL);
-}
 int total_length = 0;
 char *concatenated;
 int i, j, k = 0;
+if (ac == 0 || av == NULL)
+{
+return {NULL};
+}
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
@@ -29,7 +29,7 @@ total_length++;
 concatenated = (char *)malloc((total_length + 1) * sizeof(char));
 if (concatenated == NULL)
 {
-return (NULL);
+return {NULL};
 }
 for (i = 0; i < ac; i++)
 {
@@ -40,6 +40,6 @@ concatenated[k++] = av[i][j];
 concatenated[k++] = '\n';
 }
 concatenated[k] = '\0';
-return (concatenated);
+return {concatenated};
 }
 
