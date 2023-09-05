aa4a6a51ea072ea92e4afa01ed3910a2e8980f06
Romeo509 <romeoameku503@gmail>
2023-09-05 17:43:27 +0100

    creates an array of characters

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,15 +2,16 @@
 #include "main.h"
 
 /**
-*create_array - Creates an array of characters .
+*create_array - Creates an array of charactersr.
 *@size: The number of elements in the array.
 *@c: The character to initialize the array elements with.
 *
-*Return: A pointer to the created array or NULL if it fails.
+*Return: 0.
 */
 char *create_array(unsigned int size, char c)
 {
 char *array;
+unsigned int i = 0;
 if (size == 0)
 {
 return (0);
@@ -20,7 +21,6 @@ if (array == 0)
 {
 return (0);
 }
-unsigned int i = 0;
 while (i < size)
 {
 array[i] = c;
