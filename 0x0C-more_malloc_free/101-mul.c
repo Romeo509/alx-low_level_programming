7061cf9031b7d348e5136dd1dcd21d1c86672880
Romeo509 <romeoameku503@gmail>
2023-09-09 00:29:56 +0100

    sdfa

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,37 +2,57 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point for the program
- * @argc: The number of command-line arguments
- * @argv: An array of strings containing the arguments
- * Return: 0 for success, 98 for errors
+ * is_positive_number - Check if a string is a positive number.
+ * @str: The input string to check.
+ * Return: 1 if it's a positive number, 0 otherwise.
  */
+int is_positive_number(char *str)
+{
+if (*str == '\0')
+return (0);
+while (*str)
+{
+if (*str < '0' || *str > '9')
+return (0);
+str++;
+}
+return (1);
+}
+
+/**
+* multiply - Multiply two positive numbers.
+* @num1: The first positive number.
+* @num2: The second positive number.
+* Return: The product of num1 and num2.
+*/
+int multiply(int num1, int num2)
+{
+return (num1 * num2);
+}
+
+/**
+* main - Entry point of the program.
+* @argc: The number of command-line arguments.
+* @argv: An array of command-line arguments.
+* Return: 0 on success, 98 on error.
+*/
 int main(int argc, char *argv[])
 {
+int result, num1, num2;
 if (argc != 3)
 {
 printf("Error\n");
 return (98);
 }
-char *num1 = argv[1];
-char *num2 = argv[2];
-for (int i = 0; num1[i] != '\0'; i++)
-{
-if (num1[i] < '0' || num1[i] > '9')
+if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
 {
 printf("Error\n");
 return (98);
 }
-}
-for (int i = 0; num2[i] != '\0'; i++)
-{
-if (num2[i] < '0' || num2[i] > '9')
-{
-printf("Error\n");
-return (98);
-}
-}
-long result = atol(num1) * atol(num2);
-printf("%ld\n", result);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = multiply(num1, num2);
+printf("%d\n", result);
 return (0);
 }
+
