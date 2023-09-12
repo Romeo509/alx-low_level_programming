248cc5ff4c41da22a9d7d5920d0f30a27e7ab6b3
Romeo509 <romeoameku503@gmail>
2023-09-12 09:18:07 +0100

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
-*struct dog - the dog structure
-*@name: dog's name
-*@age: age
-*@owner: owner
-*/
-typedef struct dog
-{
-char *name;
-float *age;
-char *owner;
+struct dog {
+    char *name;
+    float age;
+    char *owner;
 };
+
+
 #endif
+
