035cf65c9b1f2bf224a809c29f8f105d20f821c4
Romeo509 <romeoameku503@gmail>
2023-09-12 09:21:43 +0100

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+*struct dog - dog itself
+*@name: name of the dog
+*@age: age of the dog
+*@owner: wner of the dog
+*/
 struct dog {
     char *name;
     float age;
