b0bdb44174a182e633bbe61b3dd0c39b3a773a55
Romeo509 <romeoameku503@gmail>
2023-09-13 23:00:31 +0100

    prints the name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,15 @@
 /**
 *print_name - Print a name using a given function
 *@name: The name to print
-*@f: A function to print the name
+*@f: A function to print the name*
+*Description:
+*This function takes a name and a function pointer and uses the provided
+*function to print the name. It ensures that the inputs are valid before
+*performing the printing operation.
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-{
+if (name == 0 || f == 0)
+return;
 f(name);
-}
 }
