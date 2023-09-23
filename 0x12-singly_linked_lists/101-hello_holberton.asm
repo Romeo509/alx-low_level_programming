6ce71ad29c38306adb12061bf684f42ba5c2d96e
Romeo509 <romeoameku503@gmail.com>
2023-09-23 22:01:12 +0100

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,15 @@
-section .data
-    msg: db "Hello, Holberton", 0
-    fmt: db "%s", 10, 0
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    global main
-
-extern printf
-
+	SECTION .text
+	extern printf
+	global main
 main:
-    mov esi, msg
-    mov edi, fmt
-    mov eax, 0
-    call printf
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-    mov eax, 60
-    xor edi, edi
-    syscall
+	mov eax, 0
+	ret
