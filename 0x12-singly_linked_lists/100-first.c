ef8ce599d056399e1b02777b3531365bbbed7052
Romeo509 <romeoameku503@gmail.com>
2023-09-23 19:56:38 +0100

    My favoourite story

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_before_main(void) __attribute__((constructor));
+
+/**
+ * print_before_main - A constructor function to print the specified text.
+ */
+void print_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
