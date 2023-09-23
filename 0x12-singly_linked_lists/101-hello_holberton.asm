28080ecaf11a5a0dd0ee0c1716900342de3ba973
Romeo509 <romeoameku503@gmail.com>
2023-09-23 20:36:55 +0100

    First assembly script

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+    hello db "Hello, Holberton",10
+
+section .text
+    global _start
+
+_start:
+    mov rax, 4
+    mov rdi, 1
+    lea rsi, [hello]
+    mov rdx, 17
+    syscall
+
+    mov rax, 1
+    xor rdi, rdi
+    syscall
