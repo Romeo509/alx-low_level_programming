187de9bbe223c0d17f79c2d266f94f1f06c42ccd
Romeo509 <romeoameku503@gmail.com>
2023-09-25 21:53:13 +0100

    fixed betty error

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,11 +11,11 @@ int pop_listint(listint_t **head)
 listint_t *temp;
 int data = 0;
 if (head == NULL || *head == NULL)
-return 0;
+return (0);
 temp = *head;
 data = temp->n;
 *head = temp->next;
 free(temp);
-return data;
+return (data);
 }
 
