1a94e1ebfd28c170f2597daf8f88d6f8160c380c
Romeo509 <romeoameku503@gmail.com>
2023-09-26 01:35:22 +0100

    n

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,49 +1,28 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t linked list safely.
- * @h: Pointer to a pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t linked list safely
+ * @h: A pointer to the head of the list
  *
- * Return: The size of the list that was freed.
+ * Return: The size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-listint_t *tortoise, *hare, *tmp;
-size_t count;
-if (h == NULL || *h == NULL)
-return (0);
-tortoise = *h;
-hare = *h;
-count = 0;
-while (tortoise && hare && hare->next)
+listint_t *current = *h;
+size_t count = 0;
+while (current != NULL)
 {
-tortoise = tortoise->next;
-hare = hare->next->next;
-if (tortoise == hare)
-{
-tortoise = *h;
-while (tortoise != hare)
-{
-tmp = tortoise;
-tortoise = tortoise->next;
-hare = hare->next;
-free(tmp);
 count++;
-}
-tmp = tortoise;
-tortoise = tortoise->next;
-free(tmp);
-count++;
-}
-}
-while (*h)
+if (current < current->next)
 {
-tmp = *h;
-*h = (*h)->next;
-free(tmp);
-count++;
-}
 *h = NULL;
+break;
+}
+*h = current->next;
+free(current);
+current = *h;
+}
 return (count);
 }
 
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,33 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the loop in a linked list.
- * @head: Pointer to the head of the list.
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: A pointer to the head of the linked list.
  *
- * Return: The address of the node where the loop starts, or NULL if there is no loop.
+ * Return: The number of nodes in the list.
  */
-listint_t *find_listint_loop(listint_t *head)
+size_t print_listint_safe(const listint_t *head)
 {
-listint_t *tortoise, *hare;
-if (head == NULL || head->next == NULL)
-return (NULL);
-tortoise = head->next;
-hare = head->next->next;
-while (hare && hare->next)
-{
-if (tortoise == hare)
-{
-tortoise = head;
-while (tortoise != hare)
-{
-tortoise = tortoise->next;
-hare = hare->next;
-}
-return (tortoise);
-}
-tortoise = tortoise->next;
-hare = hare->next->next;
-}
-return (NULL);
+    size_t count = 0;
+    const listint_t *current = head;
+    const listint_t *loop_node = NULL;
+
+    while (current)
+    {
+        printf("[%p] %d\n", (void *)current, current->n);
+        count++;
+
+        /* Check for a loop */
+        if (current >= loop_node)
+        {
+            if (loop_node == NULL)
+                loop_node = current;
+            else
+            {
+                printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
+                break;
+            }
+        }
+
+        current = current->next;
+    }
+
+    return (count);
 }
 
