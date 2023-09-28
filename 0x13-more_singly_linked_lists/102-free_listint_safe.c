5c3a739538c7d8b4b6811321a93a001e96b3e931
Romeo509 <romeoameku503@gmail.com>
2023-09-28 01:14:53 +0100

    h

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
- *
- * Return: number of elements in the freed list
- */
+* free_listint_safe - frees a linked list
+* @h: pointer to the first node in the linked list
+*
+* Return: number of elements in the freed list
+*/
 size_t free_listint_safe(listint_t **h)
 {
 size_t start = 0;
