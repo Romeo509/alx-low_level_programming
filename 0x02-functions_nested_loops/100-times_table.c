c84e4645b11c66de551fb137ebda5c9d04cc0595
Romeo509 <romeoameku503@gmail.com>
2023-10-19 16:41:21 +0100

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,38 @@
-include "main.h"
+#include "main.h"
+
 /**
- *powB - lets raises the number base to power power
- *@base : the base
- *@power : the power
- *Return: return the answer
- */
-int powB(int base, int power)
+* print_times_table - Print time table of the input
+* @n: The value
+*/
+void print_times_table(int n)
 {
-	int i, prod = 1;
-
-	for (i = 0; i < power; i++)
-	{
-		prod = prod * base;
-	}
-	return (prod);
+int num, mul, prd;
+if (n >= 0 && n <= 15)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+for (mul = 1; mul <= n; mul++)
+{
+_putchar(',');
+_putchar(' ');
+prd = num * mul;
+if (prd <= 99)
+_putchar(' ');
+if (prd <= 9)
+_putchar(' ');
+if (prd >= 100)
+{
+_putchar((prd / 100) + '0');
+_putchar(((prd / 10)) % 10 + '0');
 }
-
-/**
- *xxxxxxxxx
-
-
- 
+else if (prd <= 99 && prd >= 10)
+{
+_putchar((prd / 10) + '0');
+}
+_putchar((prd % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
