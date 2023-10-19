f68321975c1fc996527b407f121488ab7dab6a64
Romeo509 <romeoameku503@gmail.com>
2023-10-19 16:02:57 +0100

    ads

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,4 @@
+#include "header.h"
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -5,17 +6,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
 
 /**
 * check_elf - Checks if a file is an ELF file.
---
+++ 0x15-file_io/header.h
@@ -0,0 +1,16 @@
+#ifndef ELF_UTILS_H
+#define ELF_UTILS_H
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+#endif /* ELF_UTILS_H */
+
