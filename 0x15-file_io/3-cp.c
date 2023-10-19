d10f677b7cfdd3494c79a7b370ef38b499cc748f
Romeo509 <romeoameku503@gmail.com>
2023-10-19 15:51:17 +0100

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,45 @@
-#include "main.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define BUFSIZE 1024
 
 /**
-* error_file - checks if files can be opened.
-* @file_from: file_from.
-* @file_to: file_to.
-* @argv: arguments vector.
-* Return: no return.
+* exit_with_error - Helper function to display an error message and exit.
+* @code: Exit code.
+* @message: Error message.
 */
-void error_file(int file_from, int file_to, char *argv[])
+void exit_with_error(int code, const char *message)
 {
-if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+dprintf(STDERR_FILENO, "%s\n", message);
+exit(code);
 }
-if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
-}
-
-/**
-* main - check the code for Holberton School students.
-* @argc: number of arguments.
-* @argv: arguments vector.
-* Return: Always 0.
-*/
 int main(int argc, char *argv[])
 {
-int file_from, file_to, err_close;
-ssize_t nchars, nwr;
-char buf[1024];
+int fd_from, fd_to;
+ssize_t n;
+char buffer[BUFSIZE];
 if (argc != 3)
+exit_with_error(97, "Usage: cp file_from file_to");
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+exit_with_error(98, "Error: Can't read from file");
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+exit_with_error(99, "Error: Can't write to file");
+while ((n = read(fd_from, buffer, BUFSIZE)) > 0)
 {
-dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-exit(97);
+if (write(fd_to, buffer, n) != n)
+exit_with_error(99, "Error: Can't write to file");
 }
-file_from = open(argv[1], O_RDONLY);
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-error_file(file_from, file_to, argv);
-nchars = 1024;
-while (nchars == 1024)
-{
-nchars = read(file_from, buf, 1024);
-if (nchars == -1)
-error_file(-1, 0, argv);
-nwr = write(file_to, buf, nchars);
-if (nwr == -1)
-error_file(0, -1, argv);
+if (n == -1)
+exit_with_error(99, "Error: Can't write to file");
+if (close(fd_from) == -1)
+exit_with_error(100, "Error: Can't close fd");
+if (close(fd_to) == -1)
+exit_with_error(100, "Error: Can't close fd");
+return (EXIT_SUCCESS);
 }
-err_close = close(file_from);
-if (err_close == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-err_close = close(file_to);
-if (err_close == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-return (0);
-}
+
