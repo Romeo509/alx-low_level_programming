8a2d49a411944fb0a9bb8bde2bdd3db74c5bc115
Romeo509 <romeoameku503@gmail.com>
2023-10-19 17:08:23 +0100

    khadls

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,36 +1,51 @@
 #include "main.h"
 
 /**
-* print_times_table - Print time table of the input
-* @n: The value
+*print_times_table - the table
+*@n: num
+*
+*Return: 0
 */
+
 void print_times_table(int n)
 {
-int num, mul, prd;
-if (n >= 0 && n <= 15)
+int a;
+int b;
+int z;
+if (n >= 0 && n <= 14)
 {
-for (num = 0; num <= n; num++)
+for (a = 0; a <= n; a++)
 {
-_putchar('0');
-for (mul = 1; mul <= n; mul++)
+for (b = 0; b <= n; b++)
+{
+z = a * b;
+if (z > 99)
 {
 _putchar(',');
-_putchar(' ');
-prd = num * mul;
-if (prd <= 99)
-_putchar(' ');
-if (prd <= 9)
-_putchar(' ');
-if (prd >= 100)
-{
-_putchar((prd / 100) + '0');
-_putchar(((prd / 10)) % 10 + '0');
+_putchar(32);
+_putchar((z / 100) + '0');
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
 }
-else if (prd <= 99 && prd >= 10)
+else if (z > 9)
 {
-_putchar((prd / 10) + '0');
+_putchar(',');
+_putchar(32);
+_putchar(32);
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
 }
-_putchar((prd % 10) + '0');
+else
+{
+if (b != 0)
+{
+_putchar(',');
+_putchar(32);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}
 }
 _putchar('\n');
 }
