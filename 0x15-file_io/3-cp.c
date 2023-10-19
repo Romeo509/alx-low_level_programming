e4cbab1a0bf6ffa358b07a4a65acd06471e0cdc1
Romeo509 <romeoameku503@gmail.com>
2023-10-19 02:07:38 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,8 +13,12 @@
 int open_source_file(const char *filename)
 {
 int fd = open(filename, O_RDONLY);
+
 if (fd == -1)
+{
 dprintf(2, "Error: Can't read from %s\n", filename);
+}
+
 return (fd);
 }
 
@@ -27,8 +31,12 @@ return (fd);
 int open_destination_file(const char *filename)
 {
 int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
 if (fd == -1)
+{
 dprintf(2, "Error: Can't write to %s\n", filename);
+}
+
 return (fd);
 }
 
@@ -43,20 +51,24 @@ int copy_file_contents(int source_fd, int destination_fd)
 {
 char buffer[1024];
 ssize_t bytes_read, bytes_written;
-while ((bytes_read = read(source_fd, buffer, sizeof(buffer))) > 0)
+
+while ((bytes_read = read(source_fd, buffer, sizeof(buffer)) > 0)
 {
 bytes_written = write(destination_fd, buffer, bytes_read);
+
 if (bytes_written == -1)
 {
 dprintf(2, "Error: Can't write to destination file\n");
 return (-1);
 }
 }
+
 if (bytes_read == -1)
 {
 dprintf(2, "Error: Can't read from source file\n");
 return (-1);
 }
+
 return (0);
 }
 
@@ -70,9 +82,10 @@ int close_file(int fd)
 {
 if (close(fd) == -1)
 {
-dprintf(2, "Error: Can't close fd\n");
+dprintf(2, "Error: Can't close fd %d\n", fd);
 return (-1);
 }
+
 return (0);
 }
 
@@ -86,28 +99,34 @@ return (0);
 int main(int ac, char **av)
 {
 int source_fd, destination_fd;
+
 if (ac != 3)
 {
 dprintf(2, "Usage: cp file_from file_to\n");
 return (97);
 }
+
 source_fd = open_source_file(av[1]);
 if (source_fd == -1)
 return (98);
+
 destination_fd = open_destination_file(av[2]);
 if (destination_fd == -1)
 {
 close_file(source_fd);
 return (99);
 }
+
 if (copy_file_contents(source_fd, destination_fd) == -1)
 {
 close_file(source_fd);
 close_file(destination_fd);
 return (99);
 }
+
 if (close_file(source_fd) == -1 || close_file(destination_fd) == -1)
 return (100);
+
 return (0);
 }
 
