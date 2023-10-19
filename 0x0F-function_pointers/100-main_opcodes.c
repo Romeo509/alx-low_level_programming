659be9b6f210807b45b2d1583915d79e9ed25b64
Romeo509 <romeoameku503@gmail.com>
2023-10-19 02:59:20 +0100

    a

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 
 /**
-*main - Entry point
-*@argc: The number of command-line arguments
-*@argv: An array of command-line argument strings
+* main - Entry point
+* @argc: The number of command-line arguments
+* @argv: An array of command-line argument strings
 *
-*Return: 0 on success, or exit with status 1 or 2 on error
+* Return: 0 on success, or exit with status 1 or 2 on error
 */
 int main(int argc, char *argv[])
 {
 int bytes;
 char *arr;
+int i;
+
 if (argc != 2)
 {
 printf("Error\n");
@@ -23,9 +25,23 @@ if (bytes < 0)
 printf("Error\n");
 exit(2);
 }
-for (int i = 0; i < bytes; i++)
+
+arr = malloc(bytes);
+
+if (arr == NULL)
 {
-printf("%02hhx", arr[i]);
+printf("Error\n");
+exit(2);
+}
+
+for (i = 0; i < bytes; i++)
+{
+arr[i] = 0;
+}
+
+for (i = 0; i < bytes; i++)
+{
+printf("%02x", arr[i]);
 if (i == bytes - 1)
 {
 printf("\n");
@@ -35,6 +51,8 @@ else
 printf(" ");
 }
 }
+
+free(arr);
 return (0);
 }
 
