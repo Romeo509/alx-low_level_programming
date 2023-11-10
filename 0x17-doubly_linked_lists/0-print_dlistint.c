8e000cff957d48a8dae5cbfc33d6d296724d16fb
Romeo509 <romeoameku503@gmail.com>
2023-11-10 01:27:04 +0100

    g

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,13 +10,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 size_t node_count = 0;
 
-while (h)
+while (h != NULL)
 {
-node_count++;
 printf("%d\n", h->n);
 h = h->next;
+node_count++;
 }
 
 return (node_count);
 }
-
