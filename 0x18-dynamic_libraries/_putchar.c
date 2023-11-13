61a7d94ee72f6256321bb8ff8d5ec25537f23ca3
Romeo509 <romeoameku503@gmail.com>
2023-11-13 13:49:19 +0100

    */

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* _putchar - Writes a character to the standard output.
+* @c: The character to be written.
+*
+* Return: On success, returns the character written as an unsigned char
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
