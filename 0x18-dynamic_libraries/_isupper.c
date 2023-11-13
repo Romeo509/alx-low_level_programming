e1a82ce79088cdb66d141d3cbf462ac0dd0512d9
Romeo509 <romeoameku503@gmail.com>
2023-11-13 09:36:22 +0100

    int _isupper(int c);

---
+++ 0x18-dynamic_libraries/_isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isupper - Checks if a character is an uppercase letter.
+* @c: The character to be checked.
+*
+* Return: 1 if the character is an uppercase letter, 0 otherwise.
+*/
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
