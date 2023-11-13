ceb5ffe84e3593219dd078bfb9d35c1ca2eb5990
Romeo509 <romeoameku503@gmail.com>
2023-11-13 14:14:18 +0100

    int _islower(int c);

---
+++ 0x18-dynamic_libraries/_islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _islower - Checks if a character is a lowercase letter.
+* @c: The character to be checked.
+*
+* Return: 1 if the character is a lowercase letter, 0 otherwise.
+*/
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
