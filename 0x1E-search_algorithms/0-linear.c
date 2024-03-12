2f5794c12be029a18e17d13d2861f2001972561e
Romeo509 <romeoameku503@gmail.com>
2024-03-12 23:42:27 +0000

    task 1: test

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,13 +18,10 @@ return (-1);
 
 for (i = 0; i < size; i++)
 {
-printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 if (array[i] == value)
-{
-printf("Found %d at index: %lu\n", value, i);
 return (i);
 }
-}
-printf("Value not present in array\n");
+
 return (-1);
 }
