bba178d446a47b2cd9c76c01426b67e62e2be5c9
Romeo509 <romeoameku503@gmail.com>
2024-03-13 21:23:15 +0000

    errors fixed

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,31 +11,29 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-size_t low, high, mid;
+	size_t i, l, r;
 
-if (!array || size == 0)
-return (-1);
+	if (array == NULL)
+		return (-1);
 
-for (low = 0, high = size - 1; high >= low;)
-{
-mid = low + (((double)(high - low) / (array[high] - array[low])) *
-(value - array[low]));
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
 
-if (mid < size)
-printf("Checking array[%ld] = [%d]\n", mid, array[mid]);
-else
-{
-printf("Checking array[%ld] is out of range\n", mid);
-break;
-}
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
 
-if (array[mid] == value)
-return (mid);
-if (array[mid] > value)
-high = mid - 1;
-else
-low = mid + 1;
-}
-
-return (-1);
+	return (-1);
 }
